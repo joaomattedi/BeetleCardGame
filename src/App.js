b0c91ac638a6a98b36a349a0472da4fb6cb95ad2
João Mattedi <joaommattedi@gmail.com>
2022-06-27 16:12:39 -0300

    fixed some bugs

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends React.Component {
       cardAttr2: '0',
       cardAttr3: '0',
       cardImage: '',
-      cardRare: '',
+      cardRare: 'normal',
       cardTrunfo: false,
       hasTrunfo: false,
       cards: [],
